b2ba4fc73bb23cd05802425ba76c202b3aa19d68
Aniketkole27 <aniketkole07@gmail.com>
2025-01-08 18:35:11 +0530

    Refactor server setup and enhance socket.io functionality

--- serverSide.js
+++ serverSide.js
@@ -1,33 +1,45 @@
 
-const express = require('express');
-const app = express();
+const express = require('express'); 
+const app = express();   
+const PORT  = 9000;      
 const http = require('http');
 const path = require('path');
 const server = http.createServer(app);
 const { Server } = require('socket.io');
 const io = new Server(server);
 
-// make socket.io connection
+
 io.on('connection', (socket) => {
     console.log('a user connected');
     
+    socket.broadcast.emit('newPerson', "new Person is added !");
+
+    socket.on('join-room',(room)=>{
+        socket.join(room);
+        socket.broadcast.emit('joined',({message: "new user is add",room}));
+    })
+
+
     // 2nd same massage with it's name "chat-message"
     socket.on('chat-message', (msg)=>{
-        // emit that message to all using io.emit();
+        // emit that message to all , using io.emit();
         io.emit('message', msg);
     })
+
 })
 
 // app.use(express.static(path.join(__dirname, 'public')));
-app.use(express.static(path.resolve('./public')));
 
 app.get('/', (req, res) => {
-    // res.sendFile(path.join(__dirname, 'public', 'index.html'));
-    return res.sendFile('/public/index.html');
+    // res.sendFile(path.join(__dirname, 'public', 'loginPage.html'));
+    res.sendFile(path.resolve('./public/loginPage.html'));
+
+    // return res.sendFile('/public/index.html');   
 });
+app.use(express.static(path.resolve('./public')));  
 
 
 
-server.listen(9000, () => {
-    console.log('Server is running on port 9000');
+server.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
 })
