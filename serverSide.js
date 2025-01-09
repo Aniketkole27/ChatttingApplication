377b4ebd2060e4c0c87a4888d245e1e77f97c5e8
Aniketkole27 <aniketkole07@gmail.com>
2025-01-09 18:23:07 +0530

    Refactor server-side code to improve user data handling and enhance chat message functionality

--- serverSide.js
+++ serverSide.js
@@ -1,4 +1,3 @@
-
 const express = require('express'); 
 const app = express();   
 const PORT  = 9000;      
@@ -8,10 +7,13 @@ const server = http.createServer(app);
 const { Server } = require('socket.io');
 const io = new Server(server);
 
+const { sharedFunction } = require('./public/shared');
+// const name = require('./public/main');
 
+// connect to the database
 io.on('connection', (socket) => {
     console.log('a user connected');
-    
+    // console.log(sharedFunction());
     socket.broadcast.emit('newPerson', "new Person is added !");
 
     socket.on('join-room',(room)=>{
@@ -20,26 +22,46 @@ io.on('connection', (socket) => {
     })
 
 
-    // 2nd same massage with it's name "chat-message"
+  // Handle chat messages from clients 
     socket.on('chat-message', (msg)=>{
-        // emit that message to all , using io.emit();
-        io.emit('message', msg);
+        // Emit the message to all clients
+        io.emit('message', sharedFunction('Aniket',msg));
     })
+
+    function getData(name, room) {
+        console.log(`Emitting User Data: Name = ${name}, Room = ${room}`);
+                
+            io.emit('userData', { name, room });
+        
+    }
 
 })
 
-// app.use(express.static(path.join(__dirname, 'public')));
 
+// Serve the login page on the root route
 app.get('/', (req, res) => {
-    // res.sendFile(path.join(__dirname, 'public', 'loginPage.html'));
-    res.sendFile(path.resolve('./public/loginPage.html'));
+    const { name, room } = req.query; // Capture query parameters
 
-    // return res.sendFile('/public/index.html');   
+    if(name=== undefined || room === undefined){
+        
+    }
+    else{
+        console.log(`Name: ${name}, Room: ${room}`);
+        getData(name, room);
+    }
+   
+    res.sendFile(path.join(__dirname, 'public', 'loginPage.html'));
+    // res.sendFile(path.resolve('./public/loginPage.html'));
+
 });
-app.use(express.static(path.resolve('./public')));  
+
+// app.use(express.static(path.resolve('./public')));  
+app.use(express.static(path.join(__dirname, 'public')));
 
 
-
+// Start the server on port 9000
 server.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 })
+
+
